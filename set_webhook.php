94c4814c29f2b00b9a6657e053fd9094ac0a1405
Aniri2013 <aniri-13.rodina@yandex.ru>
2020-05-07 18:53:24 +0300

    My first commit.

---
+++ set_webhook.php
@@ -0,0 +1,21 @@
+<?php
+	$auth_token = '4b7be9b14727de55-cdd2b929bc3f805c-7c633555248b071d';
+	$webhook = 'WEBHOOK_URL';
+	
+	$jsonData = 
+	'{
+		"auth_token": "'.$auth_token.'",
+		"url": "'.$webhook.'",
+		"event_types": ["subscribed", "unsubscribed", "delivered", "message", "seen"]
+	}';
+	
+	$ch = curl_init('https://chatapi.viber.com/pa/set_webhook');
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+	$response = curl_exec($ch);
+	$err = curl_error($ch);
+	curl_close($ch);
+	if($err) {echo($err);}
+	else {echo($response);}
+?>
